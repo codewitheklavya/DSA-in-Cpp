382d01ecf462cad852af587b545c691ad07a6f3e
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-08-17 19:14:18 +0530

    string 2 practice

---
+++ String2/Question1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    int count = 0;
+    for(int i=0;i<=s.length();i++){
+        if(s.length()==1) break;
+        if(i==0){
+            if(s[i]!=s[i+1]) count++;
+        } 
+        else if(i==s.length()-1){
+            if(s[i]!=s[i-1]) count++;
+        }
+        else if(s[i]!=s[i-1] && s[i]!=s[i+1]){
+            count++;
+        }
+
+    }
+    cout<<endl<<count;
+    return 0;
+}
