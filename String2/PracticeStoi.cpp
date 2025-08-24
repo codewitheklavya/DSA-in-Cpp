188d1d2fda476a860e0203fc637f1b1ebb80b1d4
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-08-24 15:35:10 +0530

    also print initials zeros

--- String2/PracticeStoi.cpp
+++ String2/PracticeStoi.cpp
@@ -6,11 +6,15 @@ using namespace std;
 int main(){
     string arr[] = {"0123","0023","456","00182","980","002901"};
     int max = stoi(arr[0]);
+    string maxS = arr[0];
     int n = sizeof (arr)/sizeof(arr[0]);
     for(int i=1;i<n;i++){
         int x = stoi(arr[i]);
-        if(x>max) max=x; 
+        if(x>max){  
+            max = x;
+            maxS = arr[i];
+        }
     }
-    cout<<max;
+    cout<<maxS;
     return 0;
 }
