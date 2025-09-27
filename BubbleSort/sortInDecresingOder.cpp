fff3186dea65866e1978bbaad69782814230a44a
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-27 14:12:29 +0530

    bubble sort in decresing order

---
+++ BubbleSort/sortInDecresingOder.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {2,3,1,4,5};
+    int size = (sizeof arr/sizeof arr[0]);
+    for(int i=0;i<size-1;i++){
+        for(int j=size-1;j>0;j--){
+            if(arr[j]>arr[j-1]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i];
+    }
+    return 0;
+}
